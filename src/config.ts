66d18a3fa8edf567f523865ae6cabee3986d800e
bpond37 <bpond37@gmail.com>
2020-01-25 14:40:20 +0900

    deploy setting

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ export default {
     key: process.env.AUTH_KEY || 'bbe3007c4d11c27f0ace36dc9baf21f33be7410c13f37e292b82027348f2414b73c461ace5420c76c1be6d7ebbe256b85037cca7530ddaeb854f3baad9e6721e',
   },
   db: {
-    url: process.env.CLEARDB_DATABASE_URL || 'mysql://ulendo:ulendo@localhost:3306/note',
+    url: process.env.MYSQL_URL || 'mysql://ulendo:ulendo@localhost:3306/note',
   },
 };
